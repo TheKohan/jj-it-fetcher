c26d72ce604e0885bd279fe5a250474ab05a99c2
TheKohan <kohan142011@gmail.com>
2024-02-01 13:07:51 +0100

    feat: adjust docker script per arch

--- src/scripts/build-docker.ts
+++ src/scripts/build-docker.ts
@@ -1,6 +1,30 @@
 import { $ } from 'bun';
+import { parseArgs } from 'util';
+
+const platforms = ['amd', 'arm'] as const;
+
+type Platforms = (typeof platforms)[number];
+
+const platformConfig: Record<Platforms, string> = {
+  amd: 'linux/amd64',
+  arm: 'linux/arm64',
+};
+
+const { values } = parseArgs({
+  args: Bun.argv,
+  options: {
+    platform: {
+      type: 'string',
+      short: 'p',
+      default: 'arm',
+    },
+  },
+  strict: true,
+  allowPositionals: true,
+});
 
 const { DATABASE_URL, DISCORD_WEBHOOK_URL } = process.env;
+const { platform } = values;
 
 if (!DATABASE_URL) {
   throw new Error('DATABASE_URL Missing');
@@ -8,12 +32,18 @@ if (!DATABASE_URL) {
 if (!DISCORD_WEBHOOK_URL) {
   throw new Error('DISCORD_WEBHOOK_URL Missing');
 }
+if (!platforms.includes(platform as Platforms)) {
+  throw new Error(
+    `Unsupported platform, please use one of the following: ${platforms.join(
+      ', '
+    )}`
+  );
+}
 
-// it would be better to build the images conditionally on ARM/AMD arch
 await $`docker build \
         --tag "jj-it-fetcher" \
         --build-arg DATABASE_URL=${DATABASE_URL} \
         --build-arg DISCORD_WEBHOOK_URL=${DISCORD_WEBHOOK_URL} \
-        --platform linux/amd64 \
+        --platform ${platformConfig[platform as Platforms]} \
         -f "./Dockerfile" \
         .`;
