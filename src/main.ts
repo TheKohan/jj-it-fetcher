87b9469dc91857d8c627b3efd116704f9d00a37a
TheKohan <kohan142011@gmail.com>
2024-02-02 21:54:44 +0100

    refactor: extract cron logic to separate file

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,10 @@
-import { baseMessageEmbeds } from './discord-logger';
 import { Hono } from 'hono';
 import { logger } from 'hono/logger';
 import cron from 'node-cron';
 import { HTTPException } from 'hono/http-exception';
-import { scraperModules } from './scraper-modules/module.config';
-import prisma from './db-client';
 import { discordLogger } from './logger';
 import { apiRouter } from '@jjitfetcher/routes';
+import { scrapeAll } from './controllers';
 
 const { PORT } = process.env;
 
@@ -19,30 +17,7 @@ app.route('/', apiRouter);
  * Setup Cron Jobs.
  */
 
-cron.schedule(
-  '0 1 * * *',
-  async () => {
-    const infoEmbed = baseMessageEmbeds
-      .info()
-      .setDescription('Scrapped services: ');
-
-    for (const module of scraperModules) {
-      const offers = await module.scrape(prisma);
-      console.log(`${module.name} scrapped with:  ${offers.length} offers`);
-      if (module.withLogging) {
-        infoEmbed.addFields([
-          {
-            name: `${module.name} scrapped offers:`,
-            value: offers.length + '',
-          },
-        ]);
-      }
-    }
-
-    await discordLogger.sendInfoMessage({ message: () => infoEmbed });
-  },
-  { name: 'SCRAPE_CRON_JOB' }
-);
+cron.schedule('0 1 * * *', scrapeAll, { name: 'SCRAPE_CRON_JOB' });
 
 /**
  * Lifecycle Listeners.
@@ -64,6 +39,10 @@ app.onError(async (err, c) => {
     console.log(e);
   }
   return c.text('Failed', 500);
+});
+
+app.notFound(async c => {
+  return c.text('Page not found', 404);
 });
 
 /**
