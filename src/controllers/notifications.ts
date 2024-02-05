019f462c5cb402df176a0c3536ad032dad56cdc0
TheKohan <kohan142011@gmail.com>
2024-02-05 17:49:24 +0100

    feat: add logging for new offers daily

--- src/controllers/notifications.ts
+++ src/controllers/notifications.ts
@@ -1,6 +1,17 @@
 import { Handler } from 'hono';
 import prisma from '../db-client';
 import { DateTime } from 'luxon';
+import { baseMessageEmbeds } from '@jjitfetcher/discord-logger';
+import { discordLogger } from '../logger';
+import { ActionRowBuilder, EmbedBuilder } from 'discord.js';
+
+const TECHNOLOGIES_TAGS = [
+  'React Native',
+  'React',
+  'Javascript',
+  'TypeScript',
+  'Fullstack',
+];
 
 export const getTodayNewOffersController: Handler = async c => {
   const offers = await getTodayNewOffers();
@@ -8,13 +19,6 @@ export const getTodayNewOffersController: Handler = async c => {
 };
 
 export const getTodayNewOffers = async () => {
-  const technologies = [
-    'React Native',
-    'React',
-    'Javascript',
-    'TypeScript',
-    'Fullstack',
-  ];
   const today = DateTime.now().minus({ day: 0 }).set({
     hour: 0,
     minute: 0,
@@ -38,7 +42,9 @@ export const getTodayNewOffers = async () => {
         lt: today.toJSDate(),
       },
       AND: {
-        OR: technologies.map(tech => ({ requiredSkills: { contains: tech } })),
+        OR: TECHNOLOGIES_TAGS.map(tech => ({
+          requiredSkills: { contains: tech },
+        })),
       },
     },
     select: fieldsToSelect,
@@ -51,7 +57,9 @@ export const getTodayNewOffers = async () => {
         lt: today.plus({ day: 1 }).toJSDate(),
       },
       AND: {
-        OR: technologies.map(tech => ({ requiredSkills: { contains: tech } })),
+        OR: TECHNOLOGIES_TAGS.map(tech => ({
+          requiredSkills: { contains: tech },
+        })),
       },
     },
     select: fieldsToSelect,
@@ -60,6 +68,18 @@ export const getTodayNewOffers = async () => {
   const newOffers = todayOffers.filter(
     offer => !pastFewDaysOffers.find(yOffer => yOffer.url === offer.url)
   );
+
+  if (newOffers.length > 0) {
+    discordLogger.sendCustomMessage({
+      username: 'Daily Offers',
+      content: `New offers today: ${newOffers.length}!`,
+      avatarURL:
+        'https://upload.wikimedia.org/wikipedia/commons/4/48/Robert_Maklowicz_2014_%28cropped%29.jpg',
+      embeds: getEmbeddedOffers(newOffers),
+    });
+  } else {
+    discordLogger.sendInfoMessage({ message: 'No new offers today!' });
+  }
 
   return {
     data: {
@@ -71,4 +91,30 @@ export const getTodayNewOffers = async () => {
     pastFewDaysOffers: pastFewDaysOffers.length,
     newOffersCount: newOffers.length,
   };
+};
+
+const getEmbeddedOffers: (
+  offer: {
+    createdAt: Date;
+    title: string;
+    url: string;
+    fromPln: number;
+    toPln: number;
+    requiredSkills: string;
+  }[]
+) => EmbedBuilder[] = offers => {
+  return offers.map(offer =>
+    new EmbedBuilder()
+      .setTitle(`${offer.title}`)
+      .setColor('Orange')
+      .setFooter({ text: offer.requiredSkills })
+      .setTimestamp(Date.now())
+      .setURL(offer.url)
+      .addFields([
+        {
+          name: 'Salary: ',
+          value: `From: ${offer.fromPln} PLN, To: ${offer.toPln} PLN`,
+        },
+      ])
+  );
 };
