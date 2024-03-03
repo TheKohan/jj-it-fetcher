1912bb4ce4861739d29f63293761c1e0d0bc3183
TheKohan <kohan142011@gmail.com>
2024-03-03 14:03:57 +0100

    feat(api): add supabase jwt secret

--- apps/api/src/scripts/build-docker.ts
+++ apps/api/src/scripts/build-docker.ts
@@ -6,56 +6,60 @@ const platforms = ["amd", "arm"] as const;
 type Platforms = (typeof platforms)[number];
 
 const platformConfig: Record<Platforms, string> = {
-  amd: "linux/amd64",
-  arm: "linux/arm64",
+	amd: "linux/amd64",
+	arm: "linux/arm64",
 };
 
 const { values } = parseArgs({
-  args: Bun.argv,
-  options: {
-    platform: {
-      type: "string",
-      short: "p",
-      default: "arm",
-    },
-  },
-  strict: true,
-  allowPositionals: true,
+	args: Bun.argv,
+	options: {
+		platform: {
+			type: "string",
+			short: "p",
+			default: "arm",
+		},
+	},
+	strict: true,
+	allowPositionals: true,
 });
 
 const {
-  DIRECT_URL,
-  DATABASE_URL,
-  DISCORD_WEBHOOK_URL,
-  SUPABASE_URL,
-  SUPABASE_API_KEY,
+	DIRECT_URL,
+	DATABASE_URL,
+	DISCORD_WEBHOOK_URL,
+	SUPABASE_URL,
+	SUPABASE_API_KEY,
+	SUPABASE_JWT_SECRET,
 } = process.env;
 const { platform } = values;
 
 if (!DATABASE_URL) {
-  throw new Error("DATABASE_URL Missing");
+	throw new Error("DATABASE_URL Missing");
 }
 if (!SUPABASE_API_KEY) {
-  throw new Error("SUPABASE_API_KEY Missing");
+	throw new Error("SUPABASE_API_KEY Missing");
 }
 if (!SUPABASE_URL) {
-  throw new Error("SUPABASE_URL Missing");
+	throw new Error("SUPABASE_URL Missing");
+}
+if (!SUPABASE_JWT_SECRET) {
+	throw new Error("SUPABASE_JWT_SECRET Missing");
 }
 if (!DIRECT_URL) {
-  throw new Error("DIRECT_URL Missing");
+	throw new Error("DIRECT_URL Missing");
 }
 if (!DISCORD_WEBHOOK_URL) {
-  throw new Error("DISCORD_WEBHOOK_URL Missing");
+	throw new Error("DISCORD_WEBHOOK_URL Missing");
 }
 if (!DISCORD_WEBHOOK_URL.startsWith("https://")) {
-  throw new Error("DISCORD_WEBHOOK_URL must start with https://");
+	throw new Error("DISCORD_WEBHOOK_URL must start with https://");
 }
 if (!platforms.includes(platform as Platforms)) {
-  throw new Error(
-    `Unsupported platform, please use one of the following: ${platforms.join(
-      ", "
-    )}`
-  );
+	throw new Error(
+		`Unsupported platform, please use one of the following: ${platforms.join(
+			", ",
+		)}`,
+	);
 }
 
 await $`docker build \
@@ -65,6 +69,7 @@ await $`docker build \
         --build-arg DISCORD_WEBHOOK_URL=${DISCORD_WEBHOOK_URL} \
         --build-arg SUPABASE_URL=${SUPABASE_URL} \
         --build-arg SUPABASE_API_KEY=${SUPABASE_API_KEY} \
+        --build-arg SUPABASE_JWT_SECRET=${SUPABASE_JWT_SECRET} \
         --platform ${platformConfig[platform as Platforms]} \
         -f "./Dockerfile" \
         .`;
